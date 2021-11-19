472c48c6558bb6ff3714eefc15d88fed89ee2114
Marc Jeffrey <mjeffrey18@gmail.com>
2021-11-19 17:56:46 +0400

    moved debug stuff out of main server class
    Signed-off-by: Marc Jeffrey <mjeffrey18@gmail.com>

--- src/cable/server.cr
+++ src/cable/server.cr
@@ -16,6 +16,8 @@ module Cable
   end
 
   class Server
+    include Debug
+
     getter connections = {} of String => Connection
     getter redis_subscribe = Redis.new(url: Cable.settings.url)
     getter redis_publish = Redis.new(url: Cable.settings.url)
@@ -100,40 +102,6 @@ module Cable
       Cable::Logger.error { "IO::Error Exception: #{e.message} -> #{self.class.name}#send_to_channels(channel, message)" }
     end
 
-    def debug_json
-      _channels = {} of String => Set(String)
-
-      @channels.each do |k, v|
-        channels[v.first.class.to_s] ||= Set{k}
-        channels[v.first.class.to_s] << k
-      end
-
-      {
-        "connections"         => @connections.size,
-        "channels"            => @channels.size,
-        "channels_mounted"    => _channels,
-        "connections_mounted" => @connections.map do |key, connection|
-          connections_mounted_channels = [] of Hash(String, String | Nil)
-          @channels.each do |_, v|
-            v.each do |channel|
-              next unless channel.connection.connection_identifier == key
-              connections_mounted_channels << {
-                "channel" => channel.class.to_s,
-                "key"     => channel.stream_identifier,
-              }
-            end
-          end
-
-          {
-            "key"        => key,
-            "identifier" => connection.identifier,
-            "started_at" => connection.started_at.to_s("%Y-%m-%dT%H:%M:%S.%6N"),
-            "channels"   => connections_mounted_channels,
-          }
-        end,
-      }
-    end
-
     def safe_decode_message(message)
       case message
       when String
@@ -143,27 +111,6 @@ module Cable
       end
     rescue JSON::ParseException
       message
-    end
-
-    def debug
-      Cable::Logger.debug { "-" * 80 }
-      Cable::Logger.debug { "Some Good Information" }
-      Cable::Logger.debug { "Connections" }
-      @connections.each do |k, v|
-        Cable::Logger.debug { "Connection Key: #{k}" }
-      end
-      Cable::Logger.debug { "Channels" }
-      @channels.each do |k, v|
-        Cable::Logger.debug { "Channel Key: #{k}" }
-        Cable::Logger.debug { "Channels" }
-        v.each do |channel|
-          Cable::Logger.debug { "From Channel: #{channel.connection.connection_identifier}" }
-          Cable::Logger.debug { "Params: #{channel.params}" }
-          Cable::Logger.debug { "ID: #{channel.identifier}" }
-          Cable::Logger.debug { "Stream ID:: #{channel.stream_identifier}" }
-        end
-      end
-      Cable::Logger.debug { "-" * 80 }
     end
 
     def shutdown
