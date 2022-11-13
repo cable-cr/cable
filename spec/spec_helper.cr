fcd9dbea38f92cf5cde3fad972b5053dea746d3b
Marc Jeffrey <mjeffrey18@gmail.com>
2022-11-13 14:54:07 +0400

    added exception handler hook

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,7 @@
 require "spec"
 require "../src/cable"
 require "../src/backend/redis/backend"
+require "./support/fake_exception_service"
 require "./support/application_cable/connection"
 require "./support/application_cable/channel"
 require "./support/channels/*"
@@ -10,8 +11,12 @@ Cable.configure do |settings|
   settings.token = "test_token"
   settings.redis_ping_interval = 2.seconds
   settings.restart_error_allowance = 2
+  settings.on_error = ->(exception : Exception, message : String) do
+    FakeExceptionService.notify(exception, message: message)
+  end
 end
 
 Spec.before_each do
   Cable.restart
+  FakeExceptionService.clear
 end
