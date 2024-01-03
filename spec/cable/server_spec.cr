630915611ffb28aa9c35601f08f2183b3a2c1f9a
Rémy Marronnier <rmarronnier@gmail.com>
2024-01-03 15:49:41 +0100

    server#active_connections_for specs

--- spec/cable/server_spec.cr
+++ spec/cable/server_spec.cr
@@ -19,4 +19,37 @@ describe Cable::Server do
       end
     end
   end
+
+  describe "#active_connections_for" do
+    it "accurately returns active connections for a specificic token" do
+      Cable.reset_server
+      Cable.temp_config(backend_class: Cable::DevBackend) do
+        Cable.server.active_connections_for("abc123").size.should eq(0)
+        Cable.server.active_connections_for("def456").size.should eq(0)
+
+        socket = DummySocket.new(IO::Memory.new)
+        request = builds_request("abc123")
+        connection = ApplicationCable::Connection.new(request, socket)
+        Cable.server.add_connection(connection)
+
+        Cable.server.active_connections_for("abc123").size.should eq(1)
+
+        other_socket = DummySocket.new(IO::Memory.new)
+        other_request = builds_request("def456")
+        other_connection = ApplicationCable::Connection.new(other_request, other_socket)
+        Cable.server.add_connection(other_connection)
+
+        Cable.server.active_connections_for("def456").size.should eq(1)
+
+        connection.close
+
+        Cable.server.active_connections_for("abc123").size.should eq(0)
+        Cable.server.active_connections_for("def456").size.should eq(1)
+
+        other_connection.close
+
+        Cable.server.active_connections_for("def456").size.should eq(0)
+      end
+    end
+  end
 end
