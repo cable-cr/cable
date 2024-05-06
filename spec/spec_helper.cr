2f1360ccdd2e55c886f843421fc5b4177c1b4546
Jeremy Woertink <jeremywoertink@gmail.com>
2024-05-06 07:36:09 -0700

    Need to connect to the correct redis on CI

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -12,7 +12,8 @@ require "./support/channels/*"
 Cable.configure do |settings|
   settings.route = "/updates"
   settings.token = "test_token"
-  settings.url = "redis://localhost:6379"
+  settings.url = ENV.fetch("CABLE_BACKEND_URL", "redis://localhost:6379")
+  settings.backend_class = Cable::RedisBackend
   settings.backend_ping_interval = 2.seconds
   settings.restart_error_allowance = 2
   settings.on_error = ->(exception : Exception, message : String) do
